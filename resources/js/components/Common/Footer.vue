a57942cd1c4727e4d94904fe4222a5dba618019f
Md. Rakibul Islam <rakib1708@gmail.com>
2021-12-04 17:08:42 +0600

    updated header and footer

--- resources/js/components/Common/Footer.vue
+++ resources/js/components/Common/Footer.vue
@@ -1,38 +1,5 @@
 <template>
 	<div class="mt-3">
-		<div class="grid grid-cols-4 gap-4">
-			<div>
-				<h4 class="bar-title">About</h4>
-				<p>Foodmoy is lorem ipsum dolor sit amet for booking restaurents</p>
-			</div>
-			<div>
-				<h4 class="bar-title">Top Cuisine</h4>
-				<div class="footer-list-items">
-					<a v-for="(item, index) in libCuisines" :href="item.public_uri" class="block">
-						{{item.name}} 
-						<span class="text-xs text-gray-500">({{item.restaurents_count}})</span>
-					</a>
-				</div>
-			</div>
-			<div>
-				<h4 class="bar-title">Top Features</h4>
-				<div class="footer-list-items">
-					<a v-for="(item, index) in libFeatures" :href="item.public_uri" class="block">
-						{{item.name}} 
-						<span class="text-xs text-gray-500">({{item.restaurents_count}})</span>
-					</a>
-				</div>
-			</div>
-			<div>
-				<h4 class="bar-title">Top Categories</h4>
-				<div class="footer-list-items">
-					<a v-for="(item, index) in libCategories" :href="item.public_uri" class="block">
-						{{item.name}} 
-						<span class="text-xs text-gray-500">({{item.restaurents_count}})</span>
-					</a>
-				</div>
-			</div>
-		</div>
 		<div class="footer-section pt-3 pb-3 text-center">
 			<div class="social-media flex justify-center">
 				<a class=" mx-2 curson-pointer hover:text-red-600" href=""><i class="text-2xl lab la-facebook"></i></a>
@@ -41,7 +8,7 @@
 				<a class=" mx-2 curson-pointer hover:text-red-600" href=""><i class="text-2xl lab la-youtube"></i></a>
 			</div>
 			<p class="my-2">
-				© 2021, foodmoy. All Rights Reserved.
+				© 2021, Skeleton. All Rights Reserved.
 			</p>
 		</div>
 	</div>
--- resources/js/components/Common/Header.vue
+++ resources/js/components/Common/Header.vue
@@ -6,40 +6,16 @@
         			<div class="flex-shrink-0 flex items-center">
           				<a class="inline-flex items-center" href="/">
           					<img class="front-logo" src="/images/logo.png">
-          				</a>  
-          				     		
+          				</a>
+
           			</div>
       			</div>
 	        	<div class="hidden lg:ml-6 xl:ml-8 lg:flex lg:space-x-8 nav-menu">
-					<!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-					<a class="border-transparent text-gray-900 hover:border-red-300 hover:text-red-700 inline-flex items-center px-1 pt-1 border-b-2 text-base font-medium active" href="/">Home</a>
-					<a class="border-transparent text-gray-900 hover:border-red-300 hover:text-red-700 inline-flex items-center px-1 pt-1 border-b-2 text-base font-medium" href="/restaurents">Find Restaurents</a>
-					<a class="border-transparent text-gray-900 hover:border-red-300 hover:text-red-700 inline-flex items-center px-1 pt-1 border-b-2 text-base font-medium" href="/privacy-policy">Track Booking</a>
-					<a class="border-transparent text-gray-900 hover:border-red-300 hover:text-red-700 inline-flex items-center px-1 pt-1 border-b-2 text-base font-medium" href="/privacy-policy">Privacy Policy</a>
-					<!-- <a class="border-transparent text-gray-900 hover:border-red-300 hover:text-red-700 inline-flex items-center px-1 pt-1 border-b-2 text-base font-medium" href="/blog">Blog</a> -->
+
 	        	</div>
-      			
+
 
       			<div class="flex items-center justify-right ">
-
-      				<div class="location-search mr-3 relative">
-						<div class="relative">
-							<i class="las la-map-marker-alt absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
-							<input type="text" class="block w-full pl-6 h-8 text-sm rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:outline-none"  placeholder="search a location" v-model="keyword" @keyup="fetchLocations">
-						</div>
-						<div v-if="fetch" class="location-results absolute top-8 bg-white w-full rounded">
-							<ul v-if="locations.length > 0" class="p-3">
-								<li class="cursor-pointer mb-2 leading-4" v-for="(location, z) in locations" :key="z" @click="setLocation(location.name)">
-									{{location.name}}, 
-									<span class="font-medium">{{location.zone.name}}</span><br>
-									<span v-if="location.restaurents_count > 0" class="text-xs text-gray-500">{{location.restaurents_count}} restaurents found</span>
-								</li>
-							</ul>
-							<p v-else>No results found</p>
-							
-						</div>
-					</div>
-
       				<a v-if="!authUser" href="/login" class=" px-3 py-1 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">Login</a>
       				<div v-if="authUser">
       					<i class="text-4xl font-normal text-gray-500 las la-user-circle cursor-pointer"></i>
@@ -48,14 +24,11 @@
     		</div>
   		</div>
 
-  		<!-- Mobile menu, show/hide based on menu state. -->
   		<div class="hidden lg:hidden" id="mobile-menu" data-controller="toggle" data-toggle-target="toggleable">
     		<div class="px-2 pt-2 pb-3">
 				<a class="block px-3 py-2 rounded-xl text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" href="/">Home</a>
-				<a class="block px-3 py-2 rounded-xl text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" href="/restaurents">Book a Table</a>
 				<a class="block px-3 py-2 rounded-xl text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" href="/privacy-policy">Privacy Policy</a>
-				<!-- <a class="block px-3 py-2 rounded-xl text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" href="/blog">Blog</a> -->
-			</div> 
+			</div>
 		</div>
 	</nav>
 </template>
@@ -73,8 +46,6 @@
 		data(){
 			return {
 				keyword:'',
-				locations:[],
-				selectedLocation: '',
 				fetch: false
 			}
 		},
@@ -85,28 +56,8 @@
 		    ])
 		},
 		created(){
-			this.getCurrentLocation()
 		},
 		methods:{
-			getCurrentLocation(){
-				this.keyword = localStorage.getItem('current_location')
-			},
-			setLocation(current_location){
-				localStorage.setItem('current_location', current_location)
-				window.location.reload()
-			},
-			fetchLocations(){
-	            if(this.keyword != ''){
-					this.fetch = true;
-					axios.get('/api/fetch/locations?keyword='+this.keyword).then(({data}) => {
-		                this.locations = data;
-		            });
-	            }else{
-	            	this.fetch = false;
-	            	this.locations = [];
-	            }
-
-			}
 		}
 	}
 
